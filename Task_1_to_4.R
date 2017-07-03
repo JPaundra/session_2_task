d9dd3ee38878a4a402721740a8481c4f6bfd60a5
JPaundra <jpaundra@gmail.com>
2017-07-03 17:05:02 +0200

    Task 5 added

--- Task_1_to_4.R
+++ Task_1_to_4.R
@@ -25,3 +25,19 @@ ggplot(data= mpg, mapping = aes(x = displ, y=hwy, colour=drv))+
 ggplot(data= mpg, mapping = aes(x = displ, y=hwy, colour=cyl))+ 
   geom_point()+
   geom_smooth()
+
+# Task 5
+
+ggplot(data=mpg, mapping=aes(x=displ, y= hwy, colour=factor(cyl)))+
+  geom_point()+
+  geom_smooth(method="lm", se=FALSE)+
+  facet_wrap(~year, labeller = as_labeller(c('1999' = "Model year 1999", '2008' = "Model year 2008")))+
+  labs(x="Displacement", 
+       y="MPG, Highway",
+       colour = "Cylinders",
+       title= "Fuel economy and engine size") +
+  scale_y_log10(breaks = c(20,30,40))+
+  scale_x_log10(breaks= c(2,3,4,5,6,7))
+
+
+
