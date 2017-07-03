e68b4b4e77931a1c25fc413105f1111aed162fa6
JPaundra <jpaundra@gmail.com>
2017-07-03 16:19:59 +0200

    Task 13 added

--- Task_12_to_14.R
+++ Task_12_to_14.R
@@ -12,3 +12,26 @@ airlines %>% tbl_df
 weather %>% tbl_df
 
 # Task 12
+
+flights2<- flights %>% select(origin, year, month, day, hour,sched_dep_time, 
+                              dep_delay, carrier)
+weather2<- weather %>% select(origin, year, month, day, hour,
+                              precip, wind_speed, visib)
+
+
+inner_join(flights,weather)
+
+left_join(flights2, weather2)
+# NA is when in weather2 no result can be matched for that key
+
+# Task 13
+
+good_weather_delays<- inner_join(flights2, weather2) %>% 
+  filter(precip ==min(precip, na.rm = TRUE), 
+         wind_speed == min(wind_speed, na.rm = TRUE), 
+         visib == max(visib, na.rm = TRUE))
+
+good_weather_delays %>% 
+  group_by(carrier) %>% 
+  summarise(dep_delay=mean(dep_delay, na.rm=TRUE))
+
