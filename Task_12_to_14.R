07077fb66a4dc82296dd4e0c3a1c6e0c98c51953
JPaundra <jpaundra@gmail.com>
2017-07-03 15:45:04 +0200

    Task 12 added

---
+++ Task_12_to_14.R
@@ -0,0 +1,14 @@
+# install.packages("nycflights13")
+
+# setting library
+library(nycflights13)
+library(tidyverse)
+
+# load data
+flights %>% tbl_df
+
+airlines %>% tbl_df
+
+weather %>% tbl_df
+
+# Task 12
