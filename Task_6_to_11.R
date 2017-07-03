064c6e644fb2327e84dd79bce4c966d1cf4dfd61
JPaundra <jpaundra@gmail.com>
2017-07-03 15:11:01 +0200

    Added Task 8

--- Task_6_to_11.R
+++ Task_6_to_11.R
@@ -10,3 +10,10 @@ mpg2<- mpg %>% select(manufacturer, model, displ, year, cyl, trans, cty, hwy)
 # Task 7
 
 mpg3<- mpg2 %>% mutate(displ2=displ*displ, vol_per_cyl=displ/cyl)
+
+# Task 8
+
+mpg3 %>% filter(manufacturer == "chevrolet") %>% arrange(desc(vol_per_cyl))
+
+mpg4<- mpg3 %>% group_by(manufacturer, year) %>% summarise(vol_per_cyl =max(vol_per_cyl)) %>% rename(max_vol_per_cyl =vol_per_cyl)
+
